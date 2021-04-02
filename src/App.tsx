aaa42914b590330dc0cfc2da38b29e7aed75c144
TechnologicNick <technologicnick@gmail.com>
2021-04-02 23:04:09 +0200

    Working sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import { Sidebar } from './Sidebar';
 import './App.scss';
+// import 'halfmoon/css/halfmoon-variables.min.css';
+// import 'halfmoon';
 
 // Page components
 import PageMods from './PageMods';
 
 export interface Page {
+  category: string;
   path: string;
   displayName: string;
   component?: any;
 }
 
 export const pages: Page[] = [
-  { path: "/mods", displayName: "Mods", component: <PageMods /> },
-  { path: "/modpacks", displayName: "Modpacks" },
+  { category: "Installed", path: "/mods", displayName: "Mods", component: <PageMods /> },
+  { category: "Installed", path: "/modpacks", displayName: "Modpacks" },
+  { category: "Mod making", path: "/local-mods", displayName: "Local mods" },
 ];
 
 export function App() {
   return (
-    <>
+    <div className="page-wrapper with-sidebar">
+      <div className="sticky-alerts"></div>
       <Router>
         <Sidebar pages={pages}/>
-        <Switch>
-          {pages.map(page => (
-            <Route key={page.path} path={page.path}>
-              {page.component}
-            </Route>
-          ))}
-        </Switch>
+        <div className="content-wrapper">
+          <Switch>
+            {pages.map(page => (
+              <Route key={page.path} path={page.path}>
+                {page.component}
+              </Route>
+            ))}
+          </Switch>
+        </div>
       </Router>
-    </>
+    </div>
   );
 }
--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,15 +1,57 @@
 import React from 'react'
-import SidebarItem from './SidebarItem';
+import { NavLink } from 'react-router-dom';
+// import SidebarItem from './SidebarItem';
 import { Page } from './App';
 
-export default function Sidebar(props: { pages: Page[] }) {
+export function Sidebar(props: { pages: Page[] }) {
     return (
-        <ul>
-            {props.pages.map(page => {
-                return (
-                    <SidebarItem key={page.path} path={page.path} displayName={page.displayName} />
-                )
-            })}
-        </ul>
+        <div className="sidebar">
+            <div className="sidebar-menu">
+                <a href="#" className="sidebar-brand">
+                  {/* <img src={icon} alt="..." /> */}
+                  Totemod Launcher
+                </a>
+
+                {(() => {
+                  let out: JSX.Element[] = [];
+                  let currentCategory: string = "";
+
+                  props.pages.forEach((page: Page, index: number) => {
+                    const si = <SidebarItem key={page.path} path={page.path} displayName={page.displayName} />;
+
+                    if (currentCategory !== page.category) {
+                      currentCategory = page.category;
+                      
+                      console.log(page.category);
+
+                      out.push(<br key={page.category + '_spacing'}/>);
+                      out.push(<SidebarCategory key={page.category} displayName={page.category} />);
+                    }
+
+                    out.push(si);
+                  });
+
+                  console.log(out.map(a => a.key));
+
+                  return out;
+
+                })()}
+            </div>
+        </div>
     )
+}
+
+export function SidebarCategory(props: { displayName: string; }) {
+  return (
+    <>
+      <h5 className="sidebar-title">{props.displayName}</h5>
+      <div className="sidebar-divider"></div>
+    </>
+  )
+}
+
+export function SidebarItem(props: { path: string; displayName: string; }) {
+  return (
+      <NavLink to={props.path} className="sidebar-link" activeClassName="active">{props.displayName}</NavLink>
+  )
 }
