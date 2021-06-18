2b85320f7e3265c3b0c2742d941812fe7c39425d
TechnologicNick <technologicnick@gmail.com>
2021-06-19 01:39:15 +0200

    Add dnd rearrange

--- src/Components/ModDnd.tsx
+++ src/Components/ModDnd.tsx
@@ -4,32 +4,71 @@ import Settings from '../settings';
 import ModSearch from './ModSearch';
 import { DragDropContext, DropResult, ResponderProvided } from 'react-beautiful-dnd';
 
+function moveItem<T extends any[]>(arr: T, from: number, to: number) {
+    let clonedArr = Array.from(arr);
+    let [removed] = clonedArr.splice(from, 1);
+    clonedArr.splice(to, 0, removed);
+
+    return clonedArr;
+}
+
 export default class ModDnd extends Component {
 
-    modsDisabled!: WorkshopMod[];
-    modsEnabled!: WorkshopMod[];
+    constructor(props: {}) {
+        super(props);
+
+        // Fill lists with mods
+        let mods = Object.values(WorkshopModManager.mods).filter(mod => !mod.isFake);
+        let half = Math.floor(mods.length / 2);
+
+        this.state = {
+            modsDisabled: mods.slice(0, half - 1),
+            modsEnabled: mods.slice(half, mods.length)
+        };
+    }
+
+    state: {
+        modsDisabled: WorkshopMod[],
+        modsEnabled: WorkshopMod[],
+    } = {
+        modsDisabled: [],
+        modsEnabled: [],
+    }
 
     onDragEnd = (result: DropResult, provided: ResponderProvided) => {
         console.log(result, provided);
+
+        const { source, destination } = result;
+
+        // Dropped outside of Droppables
+        if (!destination) {
+            return;
+        }
+
+        if (source.droppableId === destination.droppableId) {
+            if (source.droppableId === "disabled") {
+                this.setState({
+                    modsDisabled: moveItem(this.state.modsDisabled, source.index, destination.index)
+                });
+            } else if (source.droppableId === "enabled") {
+                this.setState({
+                    modsEnabled: moveItem(this.state.modsEnabled, source.index, destination.index)
+                });
+            }
+        }
     }
 
     render() {
-        if (!this.modsDisabled || !this.modsEnabled) {
-            let mods = Object.values(WorkshopModManager.mods);
-            let half = Math.floor(mods.length / 2);
-
-            this.modsDisabled = mods.slice(0, half - 1);
-            this.modsEnabled = mods.slice(half, mods.length);
-        }
+        console.table(this.state);
 
         return (
             <DragDropContext onDragEnd={this.onDragEnd}>
                 <div className="d-flex" style={{ columnGap: "3rem" }}>
                     <div className="w-full">
-                        <ModSearch droppableId="disabled" mods={this.modsDisabled}/>
+                        <ModSearch droppableId="disabled" mods={this.state.modsDisabled}/>
                     </div>
                     <div className="w-full">
-                        <ModSearch droppableId="enabled" mods={this.modsEnabled}/>
+                        <ModSearch droppableId="enabled" mods={this.state.modsEnabled}/>
                     </div>
                 </div>
             </DragDropContext>
