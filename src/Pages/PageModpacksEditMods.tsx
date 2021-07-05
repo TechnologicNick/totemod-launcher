e6a874bddab8dcfbc0dfb28f50ebedf8512560a3
TechnologicNick <technologicnick@gmail.com>
2021-07-05 22:40:21 +0200

    Show half of the mods as "enabled" for testing

--- src/Pages/PageModpacksEditMods.tsx
+++ src/Pages/PageModpacksEditMods.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ModDnd from '../Components/ModDnd';
 import LoadMods from '../Components/LoadMods';
+import { WorkshopModManager } from 'scrap-mechanic-common';
 
 export default function PageModpacksEditMods() {
+    let Dnd = () => {
+                    
+        let mods = Object.values(WorkshopModManager.mods).filter(mod => !mod.isFake);
+        let half = Math.floor(mods.length / 2);
+    
+        console.log(mods.slice(half, mods.length))
+    
+        return <ModDnd modsEnabled={mods.slice(half, mods.length)}/>
+    }
+
     return (
         <div className="content">
             <h2 className="content-title">
                 Mod selection
             </h2>
             <LoadMods>
-                <ModDnd modsEnabled={[]}/>
+                <Dnd />
             </LoadMods>
         </div>
     )
