82dd3e8452fa7fe6c4f52da697f935826c58ab57
TechnologicNick <technologicnick@gmail.com>
2022-01-03 21:48:04 +0100

    Fix React errors in NodeFieldEnum

--- src/renderer/Components/NodeFieldEnum.tsx
+++ src/renderer/Components/NodeFieldEnum.tsx
@@ -7,19 +7,24 @@ const NodeFieldEnum = (props: NodeFieldProps) => {
     const property: JSONSchema7 = expandRef(props.schema, `${props.definition}/properties/${props.propertyName}`);
     const label = props.propertyName;
 
+    const selected = JSON.stringify(property.default);
+
     return (
         <div className="node-field">
             <span className="node-field-label">{label}</span>
             <div className="node-field-value-column">
-                <select className="form-control">
-                    {property.enum!.map(option => (
-                        <option
-                            value={JSON.stringify(option)}
-                            selected={option === property.default || undefined}
-                        >
-                            {option}
-                        </option>
-                    ))}
+                <select className="form-control" defaultValue={selected}>
+                    {property.enum!.map(option => {
+                        const stringified = JSON.stringify(option)
+                        return (
+                            <option
+                                key={stringified}
+                                value={stringified}
+                            >
+                                {option}
+                            </option>
+                        )
+                    })}
                 </select>
             </div>
         </div>
