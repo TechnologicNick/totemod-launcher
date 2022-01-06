36adc02baa4519d2bd08924892fcc7e9b7493e54
TechnologicNick <technologicnick@gmail.com>
2022-01-06 01:22:23 +0100

    Add NodeFieldBoolean (checkbox)

---
+++ src/renderer/Components/NodeFieldBoolean.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { expandRef } from './../schemas';
+import type { JSONSchema7 } from 'json-schema';
+import { NodeFieldProps } from './NodeField';
+import './NodeField.scss';
+
+const NodeFieldBoolean = (props: NodeFieldProps<any>) => {
+    const property: JSONSchema7 = expandRef(props.schema, `${props.definition}/properties/${props.propertyName}`);
+    const label = props.propertyName;
+
+    const selected = props.getCurrentValue?.(props.propertyName) ?? property.default;
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange?.(event.target.checked, props.propertyName);
+    }
+
+    props.onChange?.(selected, props.propertyName);
+
+    return (
+        <div className="node-field react-custom-checkbox">
+            <label className="node-field-boolean">
+                <input type={"checkbox"} defaultChecked={selected} onChange={onChange} />
+                <span />
+                {label}
+            </label>
+        </div>
+    )
+}
+
+export default NodeFieldBoolean;
