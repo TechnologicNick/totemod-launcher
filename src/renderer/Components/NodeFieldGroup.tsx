cc7284520372162dc772e892be47e07214abf411
TechnologicNick <technologicnick@gmail.com>
2022-01-30 00:08:29 +0100

    Add value to NodeFieldVector header

--- src/renderer/Components/NodeFieldGroup.tsx
+++ src/renderer/Components/NodeFieldGroup.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import './NodeField.scss';
 
-const NodeFieldGroup = (props: { label: string, children?: ReactNode }) => {
+const NodeFieldGroup = (props: { label: string, children?: ReactNode, floatRight?: ReactNode }) => {
     return (
         <details className="node-field-group rounded">
             <summary>
@@ -11,6 +11,11 @@ const NodeFieldGroup = (props: { label: string, children?: ReactNode }) => {
                 <span className="node-field-label">
                     {props.label}
                 </span>
+                {props.floatRight && (
+                    <div className="node-field-group-header-right">
+                        {props.floatRight}
+                    </div>
+                )}
             </summary>
             <div className="node-field-group-content">
                 {props.children}
--- src/renderer/Components/NodeFieldVector.tsx
+++ src/renderer/Components/NodeFieldVector.tsx
@@ -15,11 +15,21 @@ const NodeFieldVector = (props: NodeFieldProps<object>) => {
     if (!base.hasOwnProperty("$ref"))
         throw `Property "${pointer}" has no $ref property! Only references to definitions are supported.`;
 
+
+
+    let textRight: HTMLSpanElement | null;
+
+    const updateTextRight = () => {
+        if (textRight)
+            textRight.textContent = `${value.x?.toFixed(3)} ${value.y?.toFixed(3)} ${value.z?.toFixed(3)}`;
+    }
+
     const getValue = (propertyName: string) => value[propertyName];
     const setValue = (newValue: any, propertyName: string) => {
         value[propertyName] = newValue;
         console.log(`Set ${props.propertyName}.${propertyName} to`, newValue, value);
         props.onChange?.(value, props.propertyName);
+        updateTextRight();
     }
 
     const numberProps: NodeFieldProps<number> = {
@@ -31,7 +41,7 @@ const NodeFieldVector = (props: NodeFieldProps<object>) => {
     }
 
     return (
-        <NodeFieldGroup label={label}>
+        <NodeFieldGroup label={label} floatRight={<span className="float-right" ref={(ref) => { textRight = ref; updateTextRight(); }} />}>
             <NodeFieldNumber {...numberProps} propertyName="x"/>
             <NodeFieldNumber {...numberProps} propertyName="y"/>
             <NodeFieldNumber {...numberProps} propertyName="z"/>
